81a674027864eda4dfc20335356e81c5b61a1a25
utkugulbulak <98066302+utkugulbulak@users.noreply.github.com>
2022-12-01 17:18:19 -0500

    update

--- simulations/c/fit_stretches.m
+++ simulations/c/fit_stretches.m
@@ -2,7 +2,7 @@ function fit_fv_curve
 % Code fits fv_curve
 
 % Variables
-optimization_file_string = 'optimization_4D_3A.json';
+% optimization_file_string = 'optimization_4D_3A.json';
 optimization_file_string = 'optimization_3D_1A.json'
 %optimization_file_string = 'optimization_2D_1A.json'
 
